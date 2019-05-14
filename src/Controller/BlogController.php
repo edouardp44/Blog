33565139f2265c2b36d63f7a8fdd6b48a8e937e9
Edouard Provost <edouard.p44@gmail.com>
2019-05-14 13:27:16 +0200

    Routing :tada:

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,20 +3,32 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
     /**
-    * @Route("/blog", name="blog_index")
+    * @Route("/index", name="index")
     */
     public function index()
     {
         return $this->render('blog/index.html.twig', [
             'owner' => 'Edouard',
         ]);
+
+    }
+
+    /**
+     * @Route("/show/{slug}", requirements={"slug"="[a-z0-9\-]*"})
+     */
+    public function show(string $slug ="article sans titre"): Response
+    {
+        return $this->render('blog/blog.html.twig',
+            [
+                'title' => ucwords(str_replace('-',' ', $slug)),
+            ]);
 
     }
 }
